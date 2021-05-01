ebaf959211b45a8cf8509c8767b9fc82ace7b911
LeandroParisi <lepcbelisario@gmail.com>
2021-05-01 13:30:11 -0300

    feat: video fullscreen with overlayed close button

--- javaScript/homeCircles.js
+++ javaScript/homeCircles.js
@@ -2,6 +2,7 @@ const homeYellowCircle = document.querySelector('.yellow-circle')
 const playButton = document.querySelector('.play-button')
 const overlay = document.querySelector('.overlay');
 const closeButton = document.querySelector('.close-button');
+const videoPopup = document.querySelector('#video-popup')
 const video = document.querySelector('.video-container')
 
 homeYellowCircle.addEventListener('mouseover', () => {
@@ -14,21 +15,35 @@ homeYellowCircle.addEventListener('mouseover', () => {
 
 playButton.addEventListener('click', () => {
   video.autoplay = true
-  if (video.requestFullscreen) {
-    video.requestFullscreen();
+  console.log(videoPopup.requestFullscreen)
+
+  if (videoPopup.requestFullscreen) {
+    videoPopup.requestFullscreen();
   }
-  else if (video.webkitRequestFullscreen) {
-    video.webkitRequestFullscreen();
+  else if (videoPopup.webkitRequestFullscreen) {
+    videoPopup.webkitRequestFullscreen();
   }
-  else if (video.msRequestFullScreen) {
-    video.msRequestFullScreen();
+  else if (videoPopup.msRequestFullScreen) {
+    videoPopup.msRequestFullScreen();
   } 
   video.load()
+  videoPopup.classList.add('active')
   overlay.setAttribute('id', 'overlay-active')
 })
 
-closeButton.addEventListener('click', () => {
+closeButton.addEventListener('click', async () => {
   overlay.removeAttribute('id');
+  console.log(videoPopup.requestFullscreen)
+  videoPopup.classList.remove('active')
+  if (videoPopup.fullscreenElement) {
+    await videoPopup.exitFullscreen();
+  }
+  else if (videoPopup.fullscreenElement) {
+    await videoPopup.webkitexitFullscreen();
+  }
+  else if (videoPopup.fullscreenElement) {
+    await videoPopup.msexitFullscreen();
+  } 
   video.autoplay = false;
   video.pause();
 })
